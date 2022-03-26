0c8ae14d5f86a4231b5010329e4ef516c8c5ae95
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2022-03-26 18:35:02 +0800

    GameWidgetModule
    新增GameWidget基类

--- AdenClient/GameWidgetModule.cpp
+++ AdenClient/GameWidgetModule.cpp
@@ -1,1 +1,159 @@
 #include "GameWidgetModule.h"
+
+const std::function<void()> GameWidget::sm_cbEmpty = []() {};
+
+GameWidget::GameWidget()
+{
+	m_pNode = GameNodeFactory::GetInstance().CreateNode();
+
+	m_nWidth = 0, m_nHeight = 0;
+
+	m_cbOnClick = sm_cbEmpty;
+	m_cbOnEnter = sm_cbEmpty;
+	m_cbOnLeave = sm_cbEmpty;
+	m_cbOnDown = sm_cbEmpty;
+	m_cbOnUp = sm_cbEmpty;
+	m_cbOnHover = sm_cbEmpty;
+	m_cbOnPush = sm_cbEmpty;
+
+	GameMouseMotionEventManager::GetInstance().Register(
+		[&](GameEvent* pEvent)
+		{
+			GameMouseMotionEvent* pMouseMotionEvent = (GameMouseMotionEvent*)pEvent;
+
+			if (CheckCursorHit(pMouseMotionEvent->x, pMouseMotionEvent->y))
+			{
+				if (!m_bIsHover)
+				{
+					m_cbOnEnter();
+					m_cbOnHover();
+					m_bIsHover = true;
+				}
+			}
+			else
+			{
+				if (m_bIsHover)
+				{
+					m_cbOnLeave();
+					m_bIsHover = false;
+				}
+			}
+		}
+	);
+
+	GameMouseButtonEventManager::GetInstance().Register(
+		[&](GameEvent* pEvent) 
+		{
+			GameMouseButtonEvent* pMouseButtonEvent = (GameMouseButtonEvent*)pEvent;
+
+			switch (pMouseButtonEvent->emType)
+			{
+			case GameEvent::Type::MOUSE_BUTTON_DOWN:
+				if (!m_bIsDown && m_bIsHover)
+				{
+					m_cbOnDown();
+					m_cbOnPush();
+					m_bIsDown = true;
+				}
+				break;
+			case GameEvent::Type::MOUSE_BUTTON_UP:
+				if (m_bIsDown)
+				{
+					if (m_bIsHover)
+					{
+						m_cbOnClick();
+					}
+
+					m_cbOnUp();
+					m_bIsDown = false;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	);
+}
+
+bool GameWidget::CheckCursorHit(int nCursorX, int nCursorY)
+{
+	const GameNode::Point& widgetPos = m_pNode->GetPosition();
+
+	return nCursorX >= widgetPos.x && nCursorX <= widgetPos.x + m_nWidth 
+		&& nCursorY >= widgetPos.y && nCursorY <= widgetPos.y + m_nHeight;
+}
+
+GameWidget::~GameWidget()
+{
+	GameNodeFactory::GetInstance().DestroyNode(m_pNode);
+
+	m_pNode = nullptr;
+}
+
+void GameWidget::Update()
+{
+	if (m_bIsHover)
+	{
+		m_cbOnHover();
+	}
+
+	if (m_bIsDown)
+	{
+		m_cbOnPush();
+	}
+}
+
+const GameNode::Point& GameWidget::GetPosition(int& x, int& y)
+{
+	return m_pNode->GetPosition();
+}
+
+void GameWidget::GetSize(int& w, int& h)
+{
+	w = m_nWidth, h = m_nHeight;
+}
+
+void GameWidget::SetPosition(const GameNode::Point& pointPos)
+{
+	m_pNode->SetPosition(pointPos);
+}
+
+void GameWidget::SetSize(int& w, int& h)
+{
+	m_nWidth = w, m_nHeight = h;
+}
+
+void GameWidget::SetOnClick(std::function<void()> cbOnClick)
+{
+	m_cbOnClick = cbOnClick;
+}
+
+void GameWidget::SetOnEnter(std::function<void()> cbOnEnter)
+{
+	m_cbOnEnter = cbOnEnter;
+}
+
+void GameWidget::SetOnLeave(std::function<void()> cbOnLeave)
+{
+	m_cbOnLeave = cbOnLeave;
+}
+
+void GameWidget::SetOnDown(std::function<void()> cbOnDown)
+{
+	m_cbOnDown = cbOnDown;
+}
+
+void GameWidget::SetOnUp(std::function<void()> cbOnUp)
+{
+	m_cbOnUp = cbOnUp;
+}
+
+void GameWidget::SetOnHover(std::function<void()> cbHover)
+{
+	m_cbOnHover = cbHover;
+}
+
+void GameWidget::SetOnPush(std::function<void()> cbOnPush)
+{
+	m_cbOnPush = cbOnPush;
+}
--- AdenClient/GameWidgetModule.h
+++ AdenClient/GameWidgetModule.h
@@ -1,6 +1,75 @@
 #ifndef _GAME_WIDGET_MODULE_H_
 #define _GAME_WIDGET_MODULE_H_
 
+#include "GameNodeModule.h"
+#include "GameEventModule.h"
 
+#include <functional>
+
+class GameWidget
+{
+public:
+	enum class Type
+	{
+		BUTTON		= 0,
+		LABEL		= 1,
+		IMAGE		= 2,
+		SWITCH		= 3,
+		SLIDERBAR	= 4,
+		TEXTINPUT	= 5,
+		POPTIP		= 6,
+	};
+
+	virtual const GameNode::Point& GetPosition(int& x, int& y);
+	virtual void GetSize(int& w, int& h);
+
+	virtual void SetPosition(const GameNode::Point& pointPos);
+	virtual void SetSize(int& w, int& h);
+
+	virtual void SetOnClick(std::function<void()> cbOnClick);
+	virtual void SetOnEnter(std::function<void()> cbOnEnter);
+	virtual void SetOnLeave(std::function<void()> cbOnLeave);
+	virtual void SetOnDown(std::function<void()> cbOnDown);
+	virtual void SetOnUp(std::function<void()> cbOnUp);
+	virtual void SetOnHover(std::function<void()> cbHover);
+	virtual void SetOnPush(std::function<void()> cbOnPush);
+
+	virtual void Update();
+
+protected:
+	std::function<void()> m_cbOnClick;
+	std::function<void()> m_cbOnEnter;
+	std::function<void()> m_cbOnLeave;
+	std::function<void()> m_cbOnDown;
+	std::function<void()> m_cbOnUp;
+	std::function<void()> m_cbOnHover;
+	std::function<void()> m_cbOnPush;
+
+protected:
+	virtual bool CheckCursorHit(int nCursorX, int nCursorY);
+
+private:
+	GameNode*	m_pNode;
+	int			m_nWidth, m_nHeight;
+	bool		m_bIsDown, m_bIsHover;
+	static const std::function<void()> sm_cbEmpty;
+
+private:
+	GameWidget();
+	~GameWidget();
+
+	// friend class ÈÝÆ÷
+};
+
+
+class GameButton : public GameWidget
+{
+public:	
+
+
+private:
+	GameButton();
+	~GameButton();
+};
 
 #endif // !_GAME_WIDGET_MODULE_H_
